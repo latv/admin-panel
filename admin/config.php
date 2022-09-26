52582cf1183a67af30c6b0a398271ad5c485e641
Janis <feldmanis45@gmail.com>
2022-09-26 21:27:26 +0300

    format some files, fix some looks

--- admin/config.php
+++ admin/config.php
@@ -3,11 +3,11 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "projekts";
+$port = 3306; 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-?>
--- admin/index.php
+++ admin/index.php
@@ -2,44 +2,69 @@
 // Start the session
 session_start();
 ?>
-<?php include 'config.php';?>
+<?php include 'config.php'; ?>
 <!DOCTYPE html>
 <HTML lang="lv">
+
 <HEAD>
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="admin.css">
-<title>Administratora panelis</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="admin.css">
+	<title>Administratora panelis</title>
 </HEAD>
 
 <BODY>
-<div id="wrapper">
+	<div id="wrapper">
 
-<div id="header">
-<?php include 'header.php';?>
-</div>
+		<div id="header">
+			<?php include 'header.php'; ?>
+		</div>
 
-<div id="page">
-<?php
-if (!isset($_GET["action"])) {
-	echo "<h2>Esiet sveicināti administratora panelī!</h2>";	
-} else {
-	if ($_GET["action"] == 'editpages' and isset($_SESSION["SIGNED_IN"])) { include 'editpages.php'; } else if ($_GET["action"] == 'editpages' and !isset($_SESSION["SIGNED_IN"])) {include 'login.php';}
-	if ($_GET["action"] == 'blog' and isset($_SESSION["SIGNED_IN"])) { include 'blog.php';  }else if ($_GET["action"] == 'blog' and !isset($_SESSION["SIGNED_IN"])) {include 'login.php';}
-	if ($_GET["action"] == 'comment' and isset($_SESSION["SIGNED_IN"])) { include 'comment.php';  }else if ($_GET["action"] == 'comment' and !isset($_SESSION["SIGNED_IN"])) {include 'login.php';}
-	if ($_GET["action"] == 'login' and isset($_SESSION["SIGNED_IN"])) { echo "<p>Esi jau ielogojies</p>";  }else if ($_GET["action"] == 'login' and !isset($_SESSION["SIGNED_IN"])) {include 'login.php';}
-	if ($_GET["action"] == 'search' and isset($_SESSION["SIGNED_IN"])) { include 'search.php';  }else if ($_GET["action"] == 'search' and !isset($_SESSION["SIGNED_IN"])) {include 'login.php';}
-	// if ($_GET["action"] == 'search') { include 'search.php'; }
-	if ($_GET["action"] == 'install') { include 'install.php'; }
-	if ($_GET["action"] == 'signup') { include 'signup.php'; }
-	
-}
+		<div id="page">
+			<?php
+			if (!isset($_GET["action"])) {
+				echo "<h2>Esiet sveicināti administratora panelī!</h2>";
+			} else {
+				if ($_GET["action"] == 'editpages' and isset($_SESSION["SIGNED_IN"])) {
+					include 'editpages.php';
+				} else if ($_GET["action"] == 'editpages' and !isset($_SESSION["SIGNED_IN"])) {
+					include 'login.php';
+				}
+				if ($_GET["action"] == 'blog' and isset($_SESSION["SIGNED_IN"])) {
+					include 'blog.php';
+				} else if ($_GET["action"] == 'blog' and !isset($_SESSION["SIGNED_IN"])) {
+					include 'login.php';
+				}
+				if ($_GET["action"] == 'comment' and isset($_SESSION["SIGNED_IN"])) {
+					include 'comment.php';
+				} else if ($_GET["action"] == 'comment' and !isset($_SESSION["SIGNED_IN"])) {
+					include 'login.php';
+				}
+				if ($_GET["action"] == 'login' and isset($_SESSION["SIGNED_IN"])) {
+					echo "<p>Esi jau ielogojies</p>";
+				} else if ($_GET["action"] == 'login' and !isset($_SESSION["SIGNED_IN"])) {
+					include 'login.php';
+				}
+				if ($_GET["action"] == 'search' and isset($_SESSION["SIGNED_IN"])) {
+					include 'search.php';
+				} else if ($_GET["action"] == 'search' and !isset($_SESSION["SIGNED_IN"])) {
+					include 'login.php';
+				}
+				// if ($_GET["action"] == 'search') { include 'search.php'; }
+				if ($_GET["action"] == 'install') {
+					include 'install.php';
+				}
+				if ($_GET["action"] == 'signup') {
+					include 'signup.php';
+				}
+			}
 
-?>
-</div>
+			?>
+		</div>
 
-<div id="footer">
-</div>
+		<div id="footer">
+		</div>
 
-</div><!-- beidzas wrapper -->
+	</div><!-- beidzas wrapper -->
 </BODY>
+
 </HTML>
